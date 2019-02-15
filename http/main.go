8b9bacf6a3f072d7d2c9fa29f75c56844a791998
Matthew Jarvis <m.jarvis@mvoice.com.au>
2019-02-15 16:52:22 +1100

    Don't show directory listing

--- http/main.go
+++ http/main.go
@@ -3,14 +3,40 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("static")))
+	fileServer := http.FileServer(customFileSystem{http.Dir("static")})
+	mux.Handle("/", fileServer)
 
 	err := http.ListenAndServe(":8000", mux)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+}
+
+// don't show directory listings
+// attempt to show index.html otherwise return error -> 404
+// https://www.alexedwards.net/blog/disable-http-fileserver-directory-listings
+type customFileSystem struct {
+	fs http.FileSystem
+}
+
+func (cfs customFileSystem) Open(path string) (http.File, error) {
+	f, err := cfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := strings.TrimSuffix(path, "/") + "/index.html"
+		if _, err := cfs.fs.Open(index); err != nil {
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
